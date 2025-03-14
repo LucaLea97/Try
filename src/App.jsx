4be576cfcc92e99395c0121b37bc9cf17e298c87
LucaLea97 <luca.domenichelli@lea-renergy.com>
2025-03-14 12:29:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+feat/mqtt
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+import supabase from "./utils/supabase.js";
+import { useMqtt } from "./hooks/useMqtt.js";
 
 function App() {
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+    const { ledState, sendMessage } = useMqtt();
 
-  return (
-  <button>Hello world</button>
-  )
+    useEffect(() => {
+        async function checkUser() {
+            const { data, error } = await supabase.auth.getUser();
+            if (error || !data.user) {
+                navigate('/login'); // Redirect to login page
+            } else {
+                setUser(data.user);
+            }
+        }
+        checkUser();
+    }, [navigate]);
+
+    if (!user) return null; // Prevent UI flicker before redirect
+
+    async function handleLogout() {
+        await supabase.auth.signOut();
+        navigate('/login');
+    }
+
+    return (
+        <>
+            <main>
+                <h1>Clelia</h1>
+                <h3>Hello {user.email}</h3>
+                <button onClick={() => sendMessage(1, "ON")}>Turn on Led</button>
+                <button onClick={() => sendMessage(1, "OFF")}>Turn off Led</button>
+                <aside>
+                    <button> Map </button>
+                    <button> Controls </button>
+                </aside>
+                <aside style={{marginTop: '2rem'}}>
+                    <p className="text-lg">LED ID: {ledState.id || "None"}</p>
+                    <p className="text-lg">Current State: {ledState.state}</p>
+                </aside>
+                <button onClick={handleLogout}>Logout</button>
+                <footer>Lea Renergy s.r.l.</footer>
+            </main>
+        </>
+    );
 }
 
-export default App
+export default App;
